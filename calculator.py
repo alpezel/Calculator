7ccfebe334b62e1a29985a0809d8ae281550fc89
alpezel <allistairevillaruel@gmail.com>
2023-05-10 20:08:17 +0800

    pseudocode

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Ask user to choose one of the 4 math operations(+)(-)(x)(÷)
+# Ask user to input 2 numbers
+# use exeption if the input is not number; display error message and try again
+# Display result
+# Ask if the user wants to try again or not (yes or no) 
+# use exeption if the input is not yes or no; display error message and try again
+#if yes repeat program; if no display "Thank you!" 
